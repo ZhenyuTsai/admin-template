61aae676af2236b47107bcf839b5556ab787598c
zhenyutsai <zhenyutsai@foxmail.com>
2022-03-09 10:09:35 +0800

    添加生产环境清除console

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,13 @@
+const plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  // 生产环境，去掉console;保留console.err 和console.warn
+  plugins.push(['transform-remove-console', { exclude: ['error', 'warn'] }])
+}
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  plugins
 }
