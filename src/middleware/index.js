228fa7e6e4fbf6699abba388b0ae0e456ac9b223
zhenyutsai <zhenyutsai@foxmail.com>
2022-03-15 16:45:30 +0800

    添加单点登录模块

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -5,7 +5,9 @@
  * @Date 2021-10-24 09:57:51
  * @LastEditors ZhenYuTsai
  */
+import general from './general'
 import permission from './permission'
 export default (Vue) => {
   permission(Vue)
+  general(Vue)
 }
